ec84e54a643d3f23283c22afd1b5a72af06bd4fb
Zanaty <mohamed.zanaty8805@gmail.com>
2025-09-25 18:34:43 +0300

    ..

--- app/src/main/java/com/creator/movieapp/MainActivity.kt
+++ app/src/main/java/com/creator/movieapp/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.creator.movieapp
 
+import API_KEY
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.ViewModelProvider
-import com.creator.movieapp.api.API_KEY
 import com.creator.movieapp.api.RetrofitClient
 import com.creator.movieapp.model.movie.MovieRepository
 import com.creator.movieapp.model.movie.MovieViewModel
@@ -20,7 +20,6 @@ class MainActivity : ComponentActivity() {
         val api = RetrofitClient.create()
         val repo = MovieRepository(api, API_KEY)
         val factory = MovieViewModel.Factory(repo)
-
         val vm = ViewModelProvider(this, factory)[MovieViewModel::class.java]
         setContent {
             NavigationSystem(vm)
--- app/src/main/java/com/creator/movieapp/api/Base.kt
+++ app/src/main/java/com/creator/movieapp/api/Base.kt
@@ -2,3 +2,4 @@ package com.creator.movieapp.api
 
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500"
+
